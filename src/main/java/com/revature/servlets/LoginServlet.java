b792092306427e91ec283c73bddaf1ca4b88299b
John Milner <johnmilnerapple@gmail.com>
2022-03-20 13:31:09 -0700

    update web.xml for login.html form submission, add LoginServlet and ViewAllSubmissionServlet

--- src/main/java/com/revature/servlets/LoginServlet.java
+++ src/main/java/com/revature/servlets/LoginServlet.java
@@ -12,20 +12,48 @@ import java.util.List;
 
 public class LoginServlet extends HttpServlet {
 
+//    private ObjectMapper om = new ObjectMapper();
+//    private EmployeeDAO employeeDAO = new EmployeeDAOImpl();
+
     @Override // this method responds to GET requests
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // obtaining user data
-        UserService userService = new UserService();
-        List<User> users = userService.getAll();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // obtaining user data from form
+        String newUsername = req.getParameter("username");
+        String newPassword = req.getParameter("password");
+        System.out.println("We got some form data! Name = " + newUsername + " Password " + newPassword);
 
-        // use jackson-databind to convert java objects into JSON
-        ObjectMapper om = new ObjectMapper();
-        String userJson = om.writeValueAsString(users);
-
-        try (PrintWriter pw = resp.getWriter()) {
-//            pw.write(users.toString());  // instead of using the users toString, we want to use JSON
-            pw.write(userJson); // write the JSON to response body
-        }
+//        // use jackson-databind to convert java objects into JSON
+//        ObjectMapper om = new ObjectMapper();
+//        String userJson = om.writeValueAsString(users);
+//
+//        //check with username and password on server
+//        try (PrintWriter pw = resp.getWriter()) {
+////            pw.write(users.toString());  // instead of using the users toString, we want to use JSON
+//            pw.write(userJson); // write the JSON to response body
+//        }
+//
+//        Employee newEmployee = new Employee(newUsername, newPassword, null);
+//
+//        if(newEmployee.getUsername()==null){
+//            resp.sendError(400, "Email is a required field");
+//            return;
+//        }
+//
+//        if(newEmployee.getPassword()==null){
+//            resp.sendError(400, "Password is a required field");
+//            return;
+//        }
+//
+//        // add new record to the db -> use the CustomerDAO to do this
+//        boolean successfulCreation = employeeDAO.getEmployeeByUsernameAndPassword();
+//
+//        // respond to the client appropriately (HttpServletResponse)
+//        if(successfulCreation){
+//            //redirect to either submission request page or manager view all submission page
+//            resp.setStatus(201);
+//        } else {
+//            resp.sendError(500);
+//        }
+//    }
     }
-
 }
